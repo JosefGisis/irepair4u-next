d87e039cf70aa0317daab42a0e59382eed7b3bb4
josef gisis <jsquared.gisis@gmail.com>
2024-05-16 12:08:11 -0400

    added Readme

--- tina/config.ts
+++ tina/config.ts
@@ -15,35 +15,38 @@ export default defineConfig({
 				label: "Page",
 				path: "content/pages",
 				format: "md",
-				fields: [{ name: "title", type: "string" }],
-			},
-			{
-				name: "blocks",
-				label: "Blocks",
-				type: "object",
-				list: true,
-				templates: [
-                    {
-                        name: "welcomeHero",
-                        label: "Welcome Hero",
-                        fields: [
-                            { name: "message", type: "rich-text" },
-                        ],
-
-                    },
-                    {
-                        name: "links",
-                        label: "Links",
-                        type: "object",
-                        list: true,
-                        fields: [
-                            { name: "link", type: "string" },
-                            { name: "label", type: "string" },
-                            { name: "style", type: "string", options: ["primary", "secondary"]}
-                        ],
-
-
-                    }
+				fields: [
+					{ name: "title", type: "string" },
+					{
+						name: "blocks",
+						label: "Blocks",
+						type: "object",
+						list: true,
+						templates: [
+							{
+								name: "welcomeHero",
+								label: "Welcome Hero",
+								fields: [
+									{ name: "message", type: "rich-text" },
+								],
+							},
+							{
+								name: "links",
+								label: "Links",
+								type: "object",
+								list: true,
+								fields: [
+									{ name: "link", type: "string" },
+									{ name: "label", type: "string" },
+									{
+										name: "style",
+										type: "string",
+										options: ["primary", "secondary"],
+									},
+								],
+							},
+						],
+					},
 				],
 			},
 		],
