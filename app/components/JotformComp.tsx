4c4fb96d38881033f12a22a447da8db6cf0f83a7
josef gisis <jsquared.gisis@gmail.com>
2024-05-28 14:54:54 -0400

    added jotform comp

--- app/components/JotformComp.tsx
+++ app/components/JotformComp.tsx
@@ -4,7 +4,7 @@ import { Box } from '@mui/material'
 
 export default function JotformComp() {
   return (
-		<Box width="43.75rem" maxWidth="43.75rem">
+		<Box width="43.75rem">
 			<script
 				type="text/javascript"
 				src="https://form.jotform.com/jsform/233326023647048"></script>
