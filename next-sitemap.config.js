c026143dac9d8a5d7186aaa4af8ffdf1c3079e55
josef gisis <jsquared.gisis@gmail.com>
2024-06-18 12:57:55 -0400

    added next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	siteUrl: "https://www.irepair4unj.com",
+	generateRobotsTxt: true,
+	// Add paths to pages that should be included in the sitemap
+	paths: ["/", "/about", "/contact-us", "/services", "/faq"],
+	generateIndexSitemap: false,
+}
