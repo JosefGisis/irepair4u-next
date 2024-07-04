6ec3147032434f7d033387000bbf4db98d64a211
josef gisis <jsquared.gisis@gmail.com>
2024-07-04 19:29:49 -0400

    changed sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,70 @@
+/** @type {import('next-sitemap').IConfig} */
+
 module.exports = {
 	siteUrl: "https://www.irepair4unj.com",
 	generateRobotsTxt: true,
 	// Add paths to pages that should be included in the sitemap
-	paths: ["/", "/about", "/contact-us", "/services", "/faq"],
+	paths: ["/", "/about", "/contact-us", "/services", "/faq", "/README"],
 	generateIndexSitemap: false,
+	transform: async (_, path) => {
+		console.log(path)
+		switch (path) {
+			case "/": {
+				return {
+					loc: path,
+					changefreq: "monthly",
+					lastmod: new Date().toISOString(),
+					priority: 1.0,
+				}
+			}
+			case "/about": {
+				return {
+					loc: path,
+					changefreq: "monthly",
+					lastmod: new Date().toISOString(),
+					priority: 0.7,
+				}
+			}
+			case "/services": {
+				return {
+					loc: path,
+					changefreq: "monthly",
+					lastmod: new Date().toISOString(),
+					priority: 0.7,
+				}
+			}
+			case "/faq": {
+				return {
+					loc: path,
+					changefreq: "monthly",
+					lastmod: new Date().toISOString(),
+					priority: 0.7,
+				}
+			}
+			case "/contact-us": {
+				return {
+					loc: path,
+					changefreq: "monthly",
+					lastmod: new Date().toISOString(),
+					priority: 0.7,
+				}
+			}
+			case "/README": {
+				return {
+					loc: path,
+					changefreq: "monthly",
+					lastmod: new Date().toISOString(),
+					priority: 0.2,
+				}
+			}
+			default: {
+				return {
+					loc: path,
+					changefreq: "monthly",
+					lastmod: new Date().toISOString(),
+					priority: 0.7,
+				}
+			}
+		}
+	},
 }
